051c755ef52ff82a5aedac7ff0b409b59aad87ef
benjaxz3 <bzamoranos@utem.cl>
2026-04-01 11:55:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Idea de Investigación en Ciencia de Datos", layout="wide")
+st.set_page_config(page_title="Idea de Investigación en Ciencia de Datos", layout="centered")
 
 # -----------------------
 # IMAGEN INICIAL
